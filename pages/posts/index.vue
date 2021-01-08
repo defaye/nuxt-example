4e397d9017a4d8781d05dd47114f801e82b89141
Jonathan Feist <jonathan.feist@unep-wcmc.org>
2021-01-08 16:04:09 +0000

    Add example code for fetching from WP api

---
+++ pages/posts/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="posts">
+    <main>
+      <h2>Posts</h2>
+      <div
+        v-for="post in posts"
+        :key="post.id"
+        class="post"
+      >
+        <h3>
+          <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
+        </h3>
+        <div v-html="post.excerpt.rendered" />
+        <a
+          :href="`blog/${post.slug}`"
+          class="readmore"
+        >
+          Read more ⟶
+        </a>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    posts () {
+      return this.$store.state.posts
+    }
+  },
+  created () {
+    this.$store.dispatch('getPosts')
+  }
+}
+</script>
